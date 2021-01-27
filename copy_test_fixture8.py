031fc23dc8effa1656540a2e82f3d4b3689142d3
[Sergius] <[marshaloff.serg@gmail.com]>
2021-01-27 22:51:25 +0200

    My testing files

--- copy_test_fixture8.py
+++ copy_test_fixture8.py
@@ -24,3 +24,6 @@ class TestMainPage1():
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+
+# pytest -s -v -m smoke test_fixture8.py
