b74152ed125bd042b3204075e2cd06781eb5b31d
Sergius <72300097+Sergius-1488@users.noreply.github.com>
2021-02-11 12:24:16 +0200

    Update conftest.py
    new file

--- Assert_button/conftest.py
+++ Assert_button/conftest.py
@@ -1,28 +1,22 @@
 import pytest
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-# def pytest_addoption(parser):
-#         parser.addoption(
-#         "--language", action="store", default="ru", help="ru"
-#     )
+def pytest_addoption(parser):
+        parser.addoption("--language", default="ru")#, action='store', default=None, )
+
 
 @pytest.fixture(scope="function")
-def browser():
+def browser(request):
     print("\nstart browser for test..")
+    user_language=request.config.getoption("language")
+    # user_language=None
+    print(user_language)
     options = Options()
-    options.add_experimental_option('prefs',{'intl.accept_languages':'es'}) #сюда запихнуть выбранный язык 
+    options.add_experimental_option('prefs',{'intl.accept_languages': user_language})  #'es'}) #сюда запихнуть выбранный язык 
     driver = webdriver.Chrome(options=options)
     yield driver
+    time.sleep(30)
     print("\nquit browser..")
     driver.quit()
-
-
-
-
-
-
-    
-# @pytest.fixture
-# def cmdopt(request):
-#     return request.config.getoption("--language")
